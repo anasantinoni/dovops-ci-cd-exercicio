df7fba97e33d301fa0da558d08ebd2ca56fbb7dc
ana <anajuliasantinoni15@gmail.com>
2025-09-05 20:51:51 -0300

    branch

--- teste.py
+++ teste.py
@@ -1,7 +1,12 @@
 from app import soma
+from app import multiplica
 
 
 def test_soma():
     assert soma(2, 3) == 5
     assert soma(-1, 1) == 0
     assert soma(0, 0) == 0
+
+
+def test_multiplica():
+    assert multiplica(1, 1) == 0
